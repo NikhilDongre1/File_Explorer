c7c7d851abcfa2b23934ab43778d56df35f4bfbc
nikhildongre1 <91336150+NikhilDongre1@users.noreply.github.com>
2026-04-28 11:22:16 +0000

    added toggle and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import { useState } from "react";
 import "./styles.css";
-import folders from "./data.json";
+import folder from "./data.json";
 
 export default function App() {
-  const [data, setData] = useState(folders);
+  const [isExpanded, setIsExpanded] = useState({});
+  const [data, setData] = useState(folder);
 
+  const handleDelete = (file) => {
+    console.log(data);
+    const deleteNode = (nodes) => {
+      return nodes
+        .filter((node) => node.name != file.name)
+        .map((node) => {
+          if (node?.isFolder && node?.children) {
+            return {
+              ...node,
+              children: deleteNode(node.children),
+            };
+          }
+          return node;
+        });
+    };
+    setData((prev) => deleteNode(prev));
+  };
   const renderFolders = (data) => {
     return data?.map((item) => {
       return (
         <div className="nestedFolder">
-          <div>{item?.name}</div>
-          {item?.isFolder && item?.children?.length ? (
+          <div>
+            {item.isFolder ? (
+              <span
+                onClick={() =>
+                  setIsExpanded((prev) => {
+                    const newstate = { ...prev };
+                    newstate[item.name] = !newstate[item.name];
+                    return newstate;
+                  })
+                }
+              >
+                {isExpanded[item.name] ? "-" : "+"}
+              </span>
+            ) : null}
+            {item?.name}
+            <span>
+              {item?.isFolder ? (
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmcodSAtIXafKUinbJYZiaHGz1SAINoYQ-RA&s" />
+              ) : null}
+              <img
+                onClick={() => handleDelete(item)}
+                src="https://icons.veryicon.com/png/o/miscellaneous/forestry-in-yiliang/delete-316.png"
+              />
+            </span>
+          </div>
+          {item?.isFolder && item?.children?.length && isExpanded[item.name] ? (
             <div className="nestedChild">{renderFolders(item?.children)}</div>
           ) : null}
         </div>
